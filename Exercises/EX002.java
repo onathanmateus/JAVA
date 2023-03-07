f23a213ce384867dea6a182e5fc15e67ab8f5693
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-03-07 15:07:10 -0300

    I created a new java exercise

---
+++ Exercises/EX002.java
@@ -0,0 +1,12 @@
+public class EX002{
+    public static void main(String[] args){
+
+        int num = 100;
+        String nome = "Nathan";
+
+        System.out.print("Olá, mundo!\n");
+        System.out.println("Olá, mundo!");
+        System.out.printf("%s - %d", "Olá,mundo!", 2023);
+        System.out.printf("valor da variável num:%d%nNome:%s", num, nome);
+    }
+}
