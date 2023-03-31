bc7b5e3b8a82ca89f21b708a7c00bdeca2b8e278
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-03-31 11:07:14 -0300

    I updated one exercise

--- Exercises/EX009.java
+++ Exercises/EX009.java
@@ -10,7 +10,7 @@ public class EX009 {
     float n2 = entrada.nextFloat();
     System.out.println("Digite a terceira nota: ");
     float n3 = entrada.nextFloat();
-    //Calculando a média ponderada das 3 notas
+    //Calculando a média ponderada
     float m = (n1+n2+n3)/(2+3+5);
     //Mostrando o resultado na tela
     System.out.println("A média ponderada das 3 notas é de: "+m);
