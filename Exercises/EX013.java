abdfe2cfe7244a35276e04e6ef2da2258fa3d4d7
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-04-14 15:51:36 -0300

    I updated the EX013

--- Exercises/EX013.java
+++ Exercises/EX013.java
@@ -7,13 +7,13 @@ public class EX013 {
     int x;
     System.out.print("Digite um número inteiro positivo: ");
     x = ler.nextInt();
-    while (x<0){
+    while (x < 0){
       System.out.println("Por favor, digite um número inteiro positivo");
       x = ler.nextInt();
     }
     //Mostrando somente os valores inteiros pares que ficam entre 1 e o número que o usuário digitou
-    for (int i = 1; i<x; i++){
-      if (i%2==0){
+    for (int i = 1; i < x; i++){
+      if (i % 2 == 0){
         System.out.print(i + ", ");
       }
     }
