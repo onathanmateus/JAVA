e95849de0c2404ff743aa0856a2a0fba208c866f
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-05-01 09:38:03 -0300

    I organized the folder

---
+++ Exercises/Pack of exercises/EX015.java
@@ -0,0 +1,16 @@
+import java.security.SecureRandom;
+
+public class EX015 {
+  public static void main(String[] args) {
+    //Configurando e criando a matriz
+    int linhas = 5;
+    int colunas = 5;
+    int[][] matriz = new int[linhas][colunas];
+    //Preenchendo a matriz com números aleatórios
+    for(int l = 0; l < linhas; l++){
+      for(int c = 0; c < colunas; c++){
+        matriz[l][c] = new SecureRandom().nextInt(100)
+      }
+    }
+  }
+}
