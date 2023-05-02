d8c95550672e7f3c7e73278479081181bc57e0ad
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-05-02 16:57:53 -0300

    I created the EX016

---
+++ Exercises/Pack of exercises/EX016.java
@@ -0,0 +1,17 @@
+public class EX016 {
+  public static void main(String[] args) {
+    msg("Bom dia!", 5);
+    System.out.println(soma(1734617, 134761873));
+  }
+  //Classe criada para somar 2 valores
+  public static int soma(int n1, int n2){
+    int res = n1 + n2;
+    return res;
+  }
+  //Classe criada para poder gerar uma mensagem na tela 
+  public static void msg(String m, int v){
+    for(int i = 0; i < v; i++){
+      System.out.println(m);
+    }
+  }
+}
