4066136f20f828006b6d603478a5d32be45d3e8a
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-05-08 10:13:54 -0300

    I updated the EX016

--- Exercises/PackOfExercises2/EX016.java
+++ Exercises/PackOfExercises2/EX016.java
@@ -5,12 +5,12 @@ public class EX016 {
     msg("Bom dia!", 5);
     System.out.println(soma(1734617, 134761873));
   }
-  //Classe criada para somar 2 valores
+  //Método criado para somar 2 valores
   public static int soma(int n1, int n2){
     int res = n1 + n2;
     return res;
   }
-  //Classe criada para poder gerar uma mensagem na tela 
+  //Método criado para poder gerar uma mensagem na tela 
   public static void msg(String m, int v){
     for(int i = 0; i < v; i++){
       System.out.println(m);
